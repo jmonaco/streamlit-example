317ce94af955e7176ca4776d8196622896b98fe3
jmonaco <jacob.monaco@gmail.com>
2023-01-03 09:22:19 +0000

    Add charts

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,47 +16,49 @@ conn = connect(credentials=credentials)
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=600)
 def run_query(query):
-    rows = conn.execute(query, headers=0)
+    rows = conn.execute(query, headers=1)
     rows = rows.fetchall()
     return rows
 
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
-df = pd.DataFrame(rows[1:], columns=rows[0])
+st.write(f"rows.keys(): {rows.keys()}")
 
-st.markdown('## st.dataframe')
-st.dataframe(df)
+# df = pd.DataFrame(rows, columns=['Service Date', 'Service Type', 'Cost'])
 
-st.markdown('## st.line_chart')
-st.line_chart(df)
+# st.markdown('## st.dataframe')
+# st.dataframe(df)
 
-st.markdown('## st.area_chart')
-st.area_chart(df)
+# st.markdown('## st.line_chart')
+# st.line_chart(df, x="Service Date", y="Cost")
 
-st.markdown('## st.bar_chart')
-st.bar_chart(df)
+# st.markdown('## st.area_chart')
+# st.area_chart(df)
 
-st.markdown('## st.altair_chart')
-st.altair_chart(df)
+# st.markdown('## st.bar_chart')
+# st.bar_chart(df)
 
-st.markdown('## st.vega_lite_chart')
-st.vega_lite_chart(df)
+# st.markdown('## st.altair_chart')
+# st.altair_chart(df)
 
-st.markdown('## st.plotly_chart')
-st.plotly_chart(df)
+# st.markdown('## st.vega_lite_chart')
+# st.vega_lite_chart(df)
 
-st.markdown('## st.bokeh_chart')
-st.bokeh_chart(df)
+# st.markdown('## st.plotly_chart')
+# st.plotly_chart(df)
 
-st.markdown('## st.pydeck_chart')
-st.pydeck_chart(df)
+# st.markdown('## st.bokeh_chart')
+# st.bokeh_chart(df)
 
-st.markdown('## st.deck_gl_chart')
-st.deck_gl_chart(df)
+# st.markdown('## st.pydeck_chart')
+# st.pydeck_chart(df)
 
-st.markdown('## st.graphviz_chart')
-st.graphviz_chart(df)
+# st.markdown('## st.deck_gl_chart')
+# st.deck_gl_chart(df)
 
-st.markdown('## st.map')
-st.map(df)
+# st.markdown('## st.graphviz_chart')
+# st.graphviz_chart(df)
+
+# st.markdown('## st.map')
+# st.map(df)
